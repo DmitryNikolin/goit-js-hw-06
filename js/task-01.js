8e2a21306b27b67904891b4939a12e262e908832
DmitryNikolin <ni-ko-dim@ukr.net>
2022-09-16 15:59:33 +0300

    task-01

--- js/task-01.js
+++ js/task-01.js
@@ -1,23 +1,40 @@
 const categories = document.querySelectorAll('.item');
 
+console.log(' ');
 console.log(`Number of categories: ${categories.length}`);
 console.log(' ');
 
-for (const category of categories) {
+// for (const category of categories) {
+//   const group = [...category.children];
+
+categories.forEach(category => {
     const group = [...category.children];
 
-    for (const element of group) {
-        if (element.tagName === 'H2') {
-            console.log(`Category: ${element.textContent}`);
-            break;
-        }
-    }
+    // for (const element of group) {
+    //   if (element.tagName === 'H2') {
+    //     console.log(`Category: ${element.textContent}`);
+    //     break;
+    //   }
+    // }
 
-    for (const element of group) {
+    group.forEach(element => {
+        if (element.tagName === 'H2') {
+            console.log(`Elements: ${element.textContent}`);
+        }
+    });
+
+    // for (const element of group) {
+    //     if (element.tagName === 'UL') {
+    //         console.log(`Elements: ${element.children.length}`);
+    //         break;
+    //     }
+    // }
+
+    group.forEach(element => {
         if (element.tagName === 'UL') {
             console.log(`Elements: ${element.children.length}`);
-            break;
         }
-    }
+    });
     console.log(' ');
-}
+    // }
+});
