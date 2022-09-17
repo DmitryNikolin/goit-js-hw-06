f9f7478d320e892b4931ca09eb7f327285596c06
DmitryNikolin <ni-ko-dim@ukr.net>
2022-09-17 21:40:58 +0300

    task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,54 +1,54 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
 }
 
-const controlsRef = document.querySelector('#controls');
-const btnCreateRef = document.querySelector('[data-create]');
-const btnDestroyRef = document.querySelector('[data-destroy]');
-const boxesRef = document.querySelector('#boxes');
+const controls = document.querySelector('#controls');
+const btnCreate = document.querySelector('[data-create]');
+const btnDestroy = document.querySelector('[data-destroy]');
+const boxes = document.querySelector('#boxes');
 
-for (const tag of controlsRef.children) {
-  if (tag.tagName !== 'INPUT') continue;
-  tag.value = '1';
-  break;
+for (const div of controls.children) {
+    if (div.tagName !== 'INPUT') continue;
+    div.value = '1';
+    break;
 }
 
-boxesRef.style.paddingTop = '10px';
+boxes.style.paddingTop = '10px';
 
 function createBoxes(amount) {
-  const boxes = [];
+    const boxesCreated = [];
 
-  for (let i = 0; i < amount; i += 1) {
-    const box = document.createElement('div');
-    box.style.width = String(30 + 10 * i) + 'px';
-    box.style.height = String(30 + 10 * i) + 'px';
-    box.style.backgroundColor = getRandomHexColor();
-    box.style.marginBottom = '6px';
-    box.style.boxShadow = '3px 3px 3px 0px rgba(0,0,0,0.5)';
-    boxes.push(box);
-  }
+    for (let i = 0; i < amount; i += 1) {
+        const box = document.createElement('div');
+        box.style.width = String(30 + 10 * i) + 'px';
+        box.style.height = String(30 + 10 * i) + 'px';
+        box.style.backgroundColor = getRandomHexColor();
+        box.style.marginBottom = '10px';
+        box.style.boxShadow = '5px 5px 3px 2px rgba(0,0,0,0.3)';
+        boxesCreated.push(box);
+    }
 
-  boxesRef.append(...boxes);
+    boxes.append(...boxesCreated);
 }
 
 function destroyBoxes() {
-  const boxes = [...boxesRef.children];
-  for (const box of boxes) box.remove();
+    const boxesDestroyed = [...boxes.children];
+    for (const box of boxesDestroyed) box.remove();
 }
 
-btnCreateRef.addEventListener('click', () => {
-  let amount = 0;
+btnCreate.addEventListener('click', () => {
+    let amount = 0;
 
-  for (const tag of controlsRef.children) {
-    if (tag.tagName !== 'INPUT') continue;
-    amount = Number(tag.value);
-    break;
-  }
-
-  // console.log(amount);
-  createBoxes(amount);
+    for (const div of controls.children) {
+        if (div.tagName !== 'INPUT') continue;
+        amount = Number(div.value);
+        break;
+    }
+    createBoxes(amount);
 });
 
-btnDestroyRef.addEventListener('click', () => {
-  destroyBoxes();
+btnDestroy.addEventListener('click', () => {
+    destroyBoxes();
 });
